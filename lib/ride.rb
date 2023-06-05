dd557618228de18a3ab888d742c8496d118bd184
Weston Schutt <wtschutt@gmail.com>
2023-06-05 09:10:30 -0600

    Feature: Add Ride class and methods for iteration1

---
+++ lib/ride.rb
@@ -0,0 +1,18 @@
+class Ride
+  attr_reader :name, :distance, :terrain
+
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @terrain = info[:terrain]
+    @loop = info[:loop]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    @loop ? @distance : (@distance * 2)
+  end
+end
