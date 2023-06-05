d27f36992eef6151ac1a0b6ff76fbc4a885df6e3
Weston Schutt <wtschutt@gmail.com>
2023-06-05 10:18:10 -0600

    Feature: Add Biker class methods and update methods

--- lib/biker.rb
+++ lib/biker.rb
@@ -16,10 +16,26 @@ class Biker
   end
 
   def log_ride(ride, time)
+    add_ride(ride, time) if can_ride?(ride)
+  end
+
+  def can_ride?(ride)
+    ride.total_distance <= @max_distance && @acceptable_terrain.include?(ride.terrain)
+  end 
+
+  def add_ride(ride, time)
     if @rides.has_key?(ride)
       @rides[ride].push(time)
     else
       @rides[ride] = [time]
+    end
+  end
+
+  def personal_record(ride)
+    if can_ride?(ride)
+      @rides[ride].min 
+    else
+      false
     end
   end
 end
