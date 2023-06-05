8abff376e389f453da8380179b7ba3057aae1316
Weston Schutt <wtschutt@gmail.com>
2023-06-05 11:27:11 -0600

    Test: Add test for .eligible_biker(ride) method

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -61,4 +61,26 @@ RSpec.describe BikeClub do
     expect(@club.biker_best_time(ride)).not_to eq(kenny)
     expect(@club.biker_best_time(ride)).to eq(athena)
   end
+
+  it 'can determine which bikers are eligible for a given ride' do
+    kenny = Biker.new("Kenny", 30)
+      kenny.learn_terrain!(:gravel)
+      kenny.learn_terrain!(:hills)
+    athena = Biker.new("Athena", 15)
+      athena.learn_terrain!(:gravel)
+      athena.learn_terrain!(:hills)
+    sara = Biker.new("Sara", 32)
+      sara.learn_terrain!(:hills)
+    @club.add_biker(kenny)
+    @club.add_biker(athena)
+    @club.add_biker(sara)
+    
+    ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    ride3 = Ride.new({name: "Manitou Incline", distance: 18.6, loop: false, terrain: :hills})
+
+    expect(@club.eligible_bikers(ride1)).to eq([kenny, sara])
+    expect(@club.eligible_bikers(ride2)).to eq([kenny, athena])
+    expect(@club.eligible_bikers(ride3)).to eq([])
+  end
 end
